7bd6be57a66c19b2dde5b9ee3ea13538e76f6493
AM <126450800+AnneMett@users.noreply.github.com>
2024-03-14 10:33:24 +0100

    startet på bestilling af pizza

--- src/components/Pizzakort.jsx
+++ src/components/Pizzakort.jsx
@@ -1,8 +1,10 @@
-export default function Pizzakort({pizza}) {
+export default function Pizzakort({pizza, handleInput}) {
 return(
     <p>
     {pizza.nummer}. {pizza.navn} til {pizza.pris}kr. stk. <br />
-    {pizza.ingrediense} <input type="checkbox" name={pizza.nummer} value={pizza.nummer} onChange={handleInput}/> </p>
+    {pizza.ingrediense} <input type="checkbox" name={pizza.nummer}
+    value={pizza.nummer} onChange={handleInput}/> 
+    </p>
 )
 
 }
--- src/pages/PizzaPlace.jsx
+++ src/pages/PizzaPlace.jsx
@@ -12,6 +12,14 @@ const [posts, setPosts] = useState([]);
 const [isPosts, setIsPosts]= useState(true);
 
 
+const[navn, setNavn]=useState("");
+const[email, setEmail] =useState("");
+const[mobilnummer, setMobilnummer]=useState("");
+const [status, setStatus]=useState("");
+
+const[pizzaer, setPizzaer]=useState([]);
+
+
 // data hentes fra firebase go gemmes i "post" variabel
 useEffect(()=>{
     async function getPosts(){
@@ -53,7 +61,39 @@ function handleInputChange(event){
 }
 
 function handleSubmit(e) {
+    e.preventDefault();
 
+    if (pizzaer.length > 0) { // det følgende sker kun, hvis brugeren har valgt en pizza
+    const url = "https://pizzaprojects-default-rtdb.europe-west1.firebasedatabase.app/bestilling.json";
+
+    const formData ={
+        navn: navn,
+        email: email, 
+        mobilnummer: mobilnummer, 
+        pizzer: pizzaer,
+        status: "Ny bestilling"
+    };
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {'Content-Type': 'application/json',
+    },
+    })
+        .then((response) =>{
+            if (!response.ok){
+                throw new Error('Fejl ved gemning af formulardata');
+            }
+            setStatus('Bestillingen blev sendt.')
+            // console.log('Formulardata gemt i Firebase Realtime Database.');
+        })
+        .catch((error) => {
+            setStatus('Fejl ved afsendelse af bestilling' , error)
+            // console.error8'Fejl ved gemning af fomulardata', error);       
+        });
+     } else { //hvis ikke brugeren har valgt en pizza:
+    setStatus("Du skal vælge mindst én pizza fra menuen")
+}
 }
 
 
@@ -64,10 +104,39 @@ function handleSubmit(e) {
         <section className="page">
         <h1>	&#127829; PIZZA MENU 	<span>&#127829;</span></h1>
         {isPosts ? (
-            <form>
+            <form onSubmit={handleSubmit}> 
                 {posts.map((pizzaobjekt) => (
-                    <Pizzakort key={pizzaobjekt.id} pizza={pizzaobjekt} />
+                    <Pizzakort key={pizzaobjekt.id} pizza={pizzaobjekt} 
+                    handleInput={handleInputChange}/>
                 ))}
+            <input 
+                type="text"
+                name="navn"
+                placeholder="Dit navn"
+                value={navn}
+                onChange={(e)=> setNavn(e.target.value)}
+                required
+            />
+            <input 
+                type="email"
+                name="email"
+                placeholder="Din email"
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                required
+            />
+
+            <input 
+                type="number"
+                name="mobilnummer"
+                placeholder="Dit mobilnummer"
+                value={mobilnummer}
+                onChange={(e)=> setMobilnummer(e.target.value)}
+                required
+                />
+
+            {status}
+            <button type="submit">Send bestilling</button>
             </form>
         ) : (
             <p>der er ingen pizzaer at se &#128534; </p>
